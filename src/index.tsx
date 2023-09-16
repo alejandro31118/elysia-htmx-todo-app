aba6bf224076e7ae7903178ce05b4d90a9edfeb6
Alejandro Perez <alejandro31118@gmail.com>
2023-09-16 11:33:47 +0100

    Add htmx cdn

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,10 @@ const app = new Elysia()
   .use(html())
   .get('/', ({ html }) => html(
     <Html>
-      <h1>Hello from templating</h1>
+      <button hx-post='/clicked' hx-swap='outerHTML'>Click me!</button>
     </Html>
   ))
+  .post('/clicked', () => <div>I've been clicked!</div>)
   .listen(3000)
 
 console.log(
--- src/ui-components/html.tsx
+++ src/ui-components/html.tsx
@@ -7,6 +7,7 @@ export const Html = ({ children }: Children) => `
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Elysia HTMX - Todo App</title>
+        <script src="https://unpkg.com/htmx.org@1.9.5"></script>
     </head>
     <body>
         ${children}
