89421225d247b48f6f7e9e4106db91ead1550935
Alejandro Perez <alejandro31118@gmail.com>
2023-09-16 10:41:51 +0100

    Setup basic elysia server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .listen(8090)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
