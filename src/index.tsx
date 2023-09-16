b93ddfc5087b9f845c3133e39d29f6a6213a8414
Alejandro Perez <alejandro31118@gmail.com>
2023-09-16 12:05:52 +0100

    Show todos with htmx

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,27 @@ import { Elysia } from 'elysia'
 import { html } from '@elysiajs/html'
 import * as elements from 'typed-html'
 import { Html } from './ui-components/html'
+import { Todo } from './types'
+import { TodoList } from './ui-components/todo-list'
+
+const dbData: Todo[] = [
+  { id: 1, title: 'Do something', completed: false },
+  { id: 2, title: 'Go to the supermarket', description: 'Buy things', completed: true },
+]
 
 const app = new Elysia()
   .use(html())
   .get('/', ({ html }) => html(
     <Html>
-      <button hx-post='/clicked' hx-swap='outerHTML'>Click me!</button>
+      <body
+        class='min-h-screen w-screen'
+        hx-get='/todos'
+        hx-trigger='load'
+        hx-swap='innerHTML'
+      />
     </Html>
   ))
-  .post('/clicked', () => <div>I've been clicked!</div>)
+  .get('/todos', () => <TodoList todos={dbData} />)
   .listen(3000)
 
 console.log(
